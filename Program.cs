ef20336aa0ac3dc0150fe29749450d6c903c61d3
MonicaAlvesP <monica52782@gmail.com>
2024-08-18 17:42:06 -0300

    :white_check_mark: desafio concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("123456789", "Nokia N35", "1111111111", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Smartphone iphone = new Iphone("987654321", "Iphone 5", "2222222222", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
